e8200e56d0afcaef603294ce671df534052ef049
Sebastian Cortez <sebasiscort@gmail.com>
2024-02-13 22:18:34 -0500

    User Auth form component completed
    Login page v1 completed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,86 @@
 <script lang="ts">
-  //   import { GitHubLogoIcon, SymbolIcon } from "@radix-ui/react-icons";
   import { Button } from "$lib/components/ui/button";
-  import { Input } from "$lib/components/ui/input";
-  import { Label } from "$lib/components/ui/label";
-  import { cn } from "$lib/utils";
-
-  let className: string | undefined | null = undefined;
-  export { className as class };
-
-  let isLoading = false;
-  async function onSubmit() {
-    isLoading = true;
-
-    setTimeout(() => {
-      isLoading = false;
-    }, 3000);
-  }
+  import UserAuthForm from "$lib/components/UserAuthForm.svelte";
 </script>
 
-<div class={cn("grid gap-6", className)} {...$$restProps}>
-  <form on:submit|preventDefault={onSubmit}>
-    <div class="grid gap-2">
-      <div class="grid gap-1">
-        <Label class="sr-only" for="email">Email</Label>
-        <Input
-          id="email"
-          placeholder="name@example.com"
-          type="email"
-          autocapitalize="none"
-          autocomplete="email"
-          autocorrect="off"
-          disabled={isLoading}
-        />
-      </div>
-      <Button disabled={isLoading}>
-        {#if isLoading}
-          <!-- <SymbolIcon class="mr-2 h-4 w-4 animate-spin" /> -->
-        {/if}
-        Sign In with Email
-      </Button>
+<div class="md:hidden">
+  <img
+    src="/examples/authentication-light.png"
+    width={1280}
+    height={843}
+    alt="Authentication"
+    class="block dark:hidden"
+  />
+  <img
+    src="/examples/authentication-dark.png"
+    width={1280}
+    height={843}
+    alt="Authentication"
+    class="hidden dark:block"
+  />
+</div>
+<div
+  class="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+>
+  <Button
+    href="/examples/authentication"
+    variant="ghost"
+    class="absolute right-4 top-4 md:right-8 md:top-8"
+  >
+    Login
+  </Button>
+  <div
+    class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex"
+  >
+    <div
+      class="absolute inset-0 bg-cover"
+      style="
+				background-image:
+					url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80);"
+    />
+    <div class="relative z-20 flex items-center text-lg font-medium">
+      <!-- <Command class="mr-2 h-6 w-6" /> -->
+      Acme Inc
     </div>
-  </form>
-  <div class="relative">
-    <div class="absolute inset-0 flex items-center">
-      <span class="w-full border-t" />
-    </div>
-    <div class="relative flex justify-center text-xs uppercase">
-      <span class="bg-background px-2 text-muted-foreground">
-        Or continue with
-      </span>
+    <div class="relative z-20 mt-auto">
+      <blockquote class="space-y-2">
+        <p class="text-lg">
+          &ldquo;This library has saved me countless hours of work and helped me
+          deliver stunning designs to my clients faster than ever before. Highly
+          recommended!&rdquo;
+        </p>
+        <footer class="text-sm">Sofia Davis</footer>
+      </blockquote>
     </div>
   </div>
-  <Button variant="outline" type="button" disabled={isLoading}>
-    {#if isLoading}
-      <!-- <SymbolIcon class="mr-2 h-4 w-4 animate-spin" /> -->
-    {:else}
-      <!-- <GitHubLogoIcon class="mr-2 h-4 w-4" /> -->
-    {/if}
-    {" "}
-    GitHub
-  </Button>
+  <div class="lg:p-8">
+    <div
+      class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]"
+    >
+      <div class="flex flex-col space-y-2 text-center">
+        <h1 class="text-2xl font-semibold tracking-tight">Create an account</h1>
+        <p class="text-sm text-muted-foreground">
+          Enter your email below to create your account
+        </p>
+      </div>
+      <UserAuthForm />
+      <p class="px-8 text-center text-sm text-muted-foreground">
+        By clicking continue, you agree to our{" "}
+        <a
+          href="/terms"
+          class="underline underline-offset-4 hover:text-primary"
+        >
+          Terms of Service
+        </a>{" "}
+        and{" "}
+        <a
+          href="/privacy"
+          class="underline underline-offset-4 hover:text-primary"
+        >
+          Privacy Policy
+        </a>
+        .
+      </p>
+    </div>
+  </div>
 </div>
