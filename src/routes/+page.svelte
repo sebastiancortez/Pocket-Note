d06d220f19a6f5d533e3fc23f91bef85069b12e7
Sebastian Cortez <sebasiscort@gmail.com>
2024-02-14 20:42:15 -0500

    text test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,7 +45,7 @@
     <div class="relative z-20 mt-auto">
       <blockquote class="space-y-2">
         <p class="text-lg">
-          &ldquo;This library has saved me countless hours of work and helped me
+          &ldquo;This product has saved me countless hours of work and helped me
           deliver stunning designs to my clients faster than ever before. Highly
           recommended!&rdquo;
         </p>
