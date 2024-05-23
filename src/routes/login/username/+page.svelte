44ec1a0485636917b28be2c28e756a204b8e2479
Sebastian Cortez <sebasiscort@gmail.com>
2024-05-22 22:27:17 -0400

    page to create username created

---
+++ src/routes/login/username/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import { Textarea } from "$lib/components/ui/textarea";
+  import { Button } from "$lib/components/ui/button";
+
+  export let data: PageData;
+</script>
+
+<section class="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+  <Textarea placeholder="Type your message here." />
+  <Button>Send message</Button>
+</section>
