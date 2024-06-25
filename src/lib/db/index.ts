e32d1efaaebe3cfbdcb06751819076ba3b9782b1
Sebastian Cortez <sebasiscort@gmail.com>
2024-06-24 23:29:08 -0400

    Updated db folder

---
+++ src/lib/db/index.ts
@@ -0,0 +1,9 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from './schema'
+import { DATABASE_URL } from '$env/static/private'
+
+const connectionString = process.env.DATABASE_URL
+
+const client = postgres(DATABASE_URL)
+export const db = drizzle(client, {schema});
--- src/routes/login/username/+page.ts
+++ src/routes/login/username/+page.ts
@@ -1,10 +1,5 @@
 import type { PageLoad } from './$types';
 
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import { users } from '../../../schema'
-
-
 export const load = (async () => {
     return {};
 }) satisfies PageLoad;
