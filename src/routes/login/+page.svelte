46f6ff63c5531a6375789ed5d29ed5bb0e33a672
Sebastian Cortez <sebasiscort@gmail.com>
2024-06-29 23:16:46 -0400

    responsive login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -42,32 +42,9 @@
   }
 </script>
 
-<div class="md:hidden">
-  <!-- <img
-    src="/examples/authentication-light.png"
-    width={1280}
-    height={843}
-    alt="Authentication"
-    class="block dark:hidden"
-  />
-  <img
-    src="/examples/authentication-dark.png"
-    width={1280}
-    height={843}
-    alt="Authentication"
-    class="hidden dark:block"
-  /> -->
-</div>
 <div
-  class="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+  class="container relative flex h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
 >
-  <!-- <Button
-    href="/examples/authentication"
-    variant="ghost"
-    class="absolute right-4 top-4 md:right-8 md:top-8"
-  >
-    Login
-  </Button> -->
   <div
     class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex"
   >
@@ -78,7 +55,6 @@
 					url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80);"
     />
     <div class="relative z-20 flex items-center text-lg font-medium">
-      <!-- <Command class="mr-2 h-6 w-6" /> -->
       Pocket Note
     </div>
     <div class="relative z-20 mt-auto">
@@ -97,11 +73,7 @@
     >
       <div class="flex flex-col space-y-2 text-center">
         <h1 class="text-2xl font-semibold tracking-tight">Create an account</h1>
-        <!-- <p class="text-sm text-muted-foreground">
-          Enter your email below to create your account
-        </p> -->
       </div>
-      <!-- <UserAuthForm /> -->
       <Button
         on:click={() => signInWithGoogle()}
         variant="outline"
