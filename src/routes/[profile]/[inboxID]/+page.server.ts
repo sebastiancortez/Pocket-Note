7820967e33b6f876fdf502c4a0a6cfbc7dc95eb1
Sebastian Cortez <sebasiscort@gmail.com>
2024-07-13 10:21:39 -0400

    check for both username and url exist in db

--- src/routes/[profile]/[inboxID]/+page.server.ts
+++ src/routes/[profile]/[inboxID]/+page.server.ts
@@ -4,26 +4,28 @@ import { formSchema } from "./schema";
 import { zod } from "sveltekit-superforms/adapters";
 import { db } from "$lib/db";
 import { users } from "$lib/db/schema"; // Assuming inboxes are part of the users table
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 
 // Function to get the inbox by ID
-const getInboxById = async (inboxID: string) => {
-  return await db.query.users.findFirst({
-    where: eq(users.currentInboxUrl, inboxID),
- });
+const getInboxById = async (inboxID: string, username: string) => {
+    return await db.query.users.findFirst({
+      where: and(
+          eq(users.currentInboxUrl, inboxID),
+          eq(users.username, username)
+      )
+    });
 };
-
-export const load: PageServerLoad = async ({ params }) => {
-  const { inboxID } = params;
-  const inbox = await getInboxById(inboxID);
-
-  if (!inbox) {
+  
+  export const load: PageServerLoad = async ({ params }) => {
+    const { inboxID, profile: username } = params;
+    const inbox = await getInboxById(inboxID, username);
+    if (!inbox) {
+      return {
+        error: "This inbox doesn't exist or the username is incorrect",
+      };
+    }
+  
     return {
-      error: "This inbox doesn't exist",
+      form: await superValidate(zod(formSchema)),
     };
-  }
-
-  return {
-    form: await superValidate(zod(formSchema)),
   };
-};
