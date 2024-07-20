6413857f437567206a966f3fffd4262dd0dff905
Sebastian Cortez <sebasiscort@gmail.com>
2024-07-20 13:27:52 -0400

    new gradient

--- src/lib/components/scene.svelte
+++ src/lib/components/scene.svelte
@@ -10,27 +10,38 @@
   const scale = spring(1);
 
   let rotation = 0;
-  useTask((delta) => {
-    rotation += delta * 1;
-  });
-
   const noise = createNoise2D();
   const geometry = new PlaneGeometry(10, 10, 100, 100);
   const vertices = geometry.getAttribute("position").array;
 
+  // Initial noise-based z-coordinates
   for (let i = 0; i < vertices.length; i += 3) {
     const x = vertices[i];
     const y = vertices[i + 1];
-    // @ts-ignore
     vertices[i + 2] = noise(x / 4, y / 4);
   }
 
   geometry.computeVertexNormals();
+
+  useTask((delta) => {
+    rotation += delta * 1;
+
+    // Update z-coordinates to create wave effect
+    for (let i = 0; i < vertices.length; i += 3) {
+      const x = vertices[i];
+      const y = vertices[i + 1];
+      vertices[i + 2] =
+        noise(x / 4, y / 4) + Math.sin((x + rotation) / 2) * 0.5;
+    }
+
+    geometry.attributes.position.needsUpdate = true;
+    geometry.computeVertexNormals();
+  });
 </script>
 
 <T.PerspectiveCamera
   makeDefault
-  position={[10, 10, 10]}
+  position={[0, 10, 0]}
   oncreate={({ ref }) => {
     ref.lookAt(0, 1, 0);
   }}
@@ -38,13 +49,6 @@
 
 <T.DirectionalLight position={[0, 10, 10]} intensity={1.5} />
 
-<!-- <T.Mesh
-  position.y={1}
-  scale={$scale}
-  onpointerenter={() => scale.set(1.5)}
-  onpointerout={() => scale.set(1)}
-> -->
-<!-- <T.BoxGeometry args={[1, 2, 1]} /> -->
 <T.Mesh {geometry} rotation.x={DEG2RAD * -90}>
   <T.MeshNormalMaterial />
 </T.Mesh>
